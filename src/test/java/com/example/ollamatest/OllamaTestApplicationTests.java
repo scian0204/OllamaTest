38b3e5dfabcff25325dec34df6b7f4d9000a0aa9
scian0204 <scian0204@gmail.com>
2024-04-25 14:30:22 +0900

    프로젝트 생성 및 기본 설정

---
+++ src/test/java/com/example/ollamatest/OllamaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ollamatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
