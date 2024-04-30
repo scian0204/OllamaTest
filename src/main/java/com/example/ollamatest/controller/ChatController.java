68d18ccc56a3ba91ed75ae0e3c2b392e6e7a28ed
scian0204 <scian0204@gmail.com>
2024-04-30 13:28:32 +0900

    사용하지 않는 코드 삭제

--- src/main/java/com/example/ollamatest/controller/ChatController.java
+++ src/main/java/com/example/ollamatest/controller/ChatController.java
@@ -9,7 +9,6 @@ import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Sinks;
 
 import java.util.stream.Collectors;
 
@@ -17,23 +16,12 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/chat")
 @RequiredArgsConstructor
 public class ChatController {
-    final private Sinks.Many<ChatResponse> chatSink = Sinks.many().multicast().onBackpressureBuffer();
 
     final private OllamaChatClient chatClient;
 
     @PostMapping(value = "/generate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ChatResponse> generate(@RequestBody ChatDto chatDto) {
         Prompt prompt = new Prompt(chatDto.getChatObjectList().stream().map(CustomMessage::new).collect(Collectors.toList()));
-//        return chatClient.stream(prompt).doOnNext(chatSink::tryEmitNext);
         return chatClient.stream(prompt);
-//                .map(e->e.getResult().getOutput().getContent());
-    }
-
-    @GetMapping("/stream")
-    public Flux<ChatResponse> generateStream() {
-        return chatSink.asFlux()
-                .doOnCancel(() -> {
-                    chatSink.asFlux().blockLast();
-                });
     }
 }
