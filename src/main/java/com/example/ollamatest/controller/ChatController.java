14a242291e34fa935c68753352b307ae0fb4468b
scian0204 <scian0204@gmail.com>
2024-04-30 13:26:29 +0900

    대화 기억 기능 추가

--- src/main/java/com/example/ollamatest/controller/ChatController.java
+++ src/main/java/com/example/ollamatest/controller/ChatController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
@@ -20,10 +21,12 @@ public class ChatController {
 
     final private OllamaChatClient chatClient;
 
-    @PostMapping("/generate")
+    @PostMapping(value = "/generate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ChatResponse> generate(@RequestBody ChatDto chatDto) {
         Prompt prompt = new Prompt(chatDto.getChatObjectList().stream().map(CustomMessage::new).collect(Collectors.toList()));
-        return chatClient.stream(prompt).doOnNext(chatSink::tryEmitNext);
+//        return chatClient.stream(prompt).doOnNext(chatSink::tryEmitNext);
+        return chatClient.stream(prompt);
+//                .map(e->e.getResult().getOutput().getContent());
     }
 
     @GetMapping("/stream")
